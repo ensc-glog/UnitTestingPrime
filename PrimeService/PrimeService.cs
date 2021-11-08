5cfbd1557face4cb6722fc59e3ff4108a70a8b74
bpesquet <bpesquet@gmail.com>
2021-11-08 21:55:19 +0100

    App creation

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            // Incomplete implementations
+            //throw new NotImplementedException("Not implemented.");
+            //return false;
+
+            if (candidate < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
